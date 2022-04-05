2d7b2cb57eb8aa041f4fb5296043142a9937e8aa
hbbtOfLight <kataryna.kazinskaya@gmail.com>
2022-04-05 23:55:31 +0300

    Rm local build folder

--- build/src/CMakeFiles/labrabota5t2-h2o.main.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for labrabota5t2-h2o.main.
--- build/test/CMakeFiles/labrabota5t2-h2o.test.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for labrabota5t2-h2o.test.
-# This may be replaced when dependencies are built.
