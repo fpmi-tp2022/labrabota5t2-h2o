1c00ec8c8936d1b434d33bc4bb1745386de113db
hbbtOfLight <kataryna.kazinskaya@gmail.com>
2022-04-05 14:54:21 +0300

    Const casting mb fixed

--- test/test_functional.cpp
+++ test/test_functional.cpp
@@ -17,10 +17,10 @@ TEST(h2o_testing, testInsert) {
     sqlite3 *db = open_data_base();
     EXPECT_NE(db, nullptr);
     std::string sql = "INSERT INTO PARK_DRIVERS VALUES (7, 'KERRIGAN', '1', '1000', 'Mirror', '2017' );";
-    char* s_data = sql.data();
+    char* s_data = const_cast<char*>(sql.data());
     EXPECT_NO_THROW(insert_data(db, s_data));
     sql = "SELECT driver_id FROM park_drivers WHERE driver_id = 7;";
-    s_data = sql.data();
+    s_data = const_cast<char*>(sql.data());
     int selected = select_data(db, s_data);
     EXPECT_EQ(selected, 1);
     sqlite3_close(db);
@@ -30,10 +30,10 @@ TEST(h2o_testing, testDelete) {
     sqlite3 *db = open_data_base();
     EXPECT_NE(db, nullptr);
     std::string sql = "DELETE FROM park_drivers WHERE driver_id = 7;";
-    char* s_data = sql.data();
+    char* s_data = const_cast<char*>(sql.data());
     EXPECT_NO_THROW(insert_data(db, s_data));
     sql = "SELECT driver_id FROM park_drivers WHERE driver_id = 7;";
-    s_data = sql.data();
+    s_data = const_cast<char*>(sql.data());
     int selected = select_data(db, s_data);
     EXPECT_EQ(selected, 0);
     sqlite3_close(db);
@@ -43,7 +43,7 @@ TEST(h2o_testing, testSelectByDrivers) {
     sqlite3 *db = open_data_base();
     EXPECT_NE(db, nullptr);
     std::string driver = "Lohnesskiy";
-    char* s_data = driver.data();
+    char* s_data = const_cast<char*>(driver.data());
     int selected = select_by_driver(db, s_data);
     EXPECT_EQ(selected, 0);
     sqlite3_close(db);
@@ -53,7 +53,7 @@ TEST(h2o_testing, testSelectByCar) {
     sqlite3 *db = open_data_base();
     EXPECT_NE(db, nullptr);
     std::string driver = "Lohnesskiy", car = "1";
-    char* s_data = driver.data(), *c_data = car.data();
+    char* s_data = const_cast<char*>(driver.data()), *c_data = const_cast<char*>(car.data());
     int selected = select_by_car(db, c_data, s_data);
     EXPECT_EQ(selected, 0);
     sqlite3_close(db);
@@ -63,7 +63,7 @@ TEST(h2o_testing, testSelectByEachDriver) {
     sqlite3 *db = open_data_base();
     EXPECT_NE(db, nullptr);
     std::string driver = "Lohnesskiy";
-    char* s_data = driver.data();
+    char* s_data = const_cast<char*>(driver.data());
     int selected = select_by_each_driver(db, s_data);
     EXPECT_EQ(selected, 0);
     sqlite3_close(db);
